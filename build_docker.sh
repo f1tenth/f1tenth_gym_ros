53e50e81da27f8ea5b1c24fc0d1149af0c5e8a90
Benjamin Evans <31577482+BDEvan5@users.noreply.github.com>
2021-05-27 12:39:54 +0200

    Checkout correct branch of f1tenth_gym repo
    Fixed file according to solution listed in issue tracker.

--- build_docker.sh
+++ build_docker.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 if [ ! -d f1tenth_gym ] ; then
     git clone https://github.com/f1tenth/f1tenth_gym
+    cd f1tenth_gym
+    git checkout cpp_backend_archive
+    cd ../
 else
     echo f1tenth_gym exists, not cloning.
 fi
