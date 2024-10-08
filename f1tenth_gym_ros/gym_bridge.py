08470fdc16114c190acc91fe2e78e427cf214cc2
Ahmad Amine <ahmadamine998@gmail.com>
2024-10-08 04:00:49 -0400

    Use original observation and update reset

--- f1tenth_gym_ros/gym_bridge.py
+++ f1tenth_gym_ros/gym_bridge.py
@@ -94,7 +94,7 @@ class GymBridge(Node):
         path = self.get_parameter('map_path').value
         name = path.split('/')[-1].split('.')[0]
         path = path + '.yaml'
-        self.get_logger().error('Loading map: %s from path: %s' % (name, path))
+        self.get_logger().info('Loading map: %s from path: %s' % (name, path))
 
         # Load the yaml file
         path = pathlib.Path(path)
@@ -110,7 +110,7 @@ class GymBridge(Node):
                                 "integrator": "rk4",
                                 "control_input": ["speed", "steering_angle"],
                                 "model": "st",
-                                "observation_config": {"type": "kinematic_state"},
+                                "observation_config": {"type": "original"},
                                 "params": vehicle_params,
                                 "reset_config": {"type": "map_random_static"},
                                 "scale": scale,
@@ -148,7 +148,7 @@ class GymBridge(Node):
             self.opp_requested_speed = 0.0
             self.opp_steer = 0.0
             self.opp_collision = False
-            self.obs, _ , self.done, _ = self.env.reset(np.array([[sx, sy, stheta], [sx1, sy1, stheta1]]))
+            self.obs, _ = self.env.reset(options={"poses": np.array([[sx, sy, stheta], [sx1, sy1, stheta1]])})
             self.ego_scan = list(self.obs['scans'][0])
             self.opp_scan = list(self.obs['scans'][1])
 
@@ -160,7 +160,7 @@ class GymBridge(Node):
             opp_ego_odom_topic = self.opp_namespace + '/' + self.get_parameter('opp_ego_odom_topic').value
         else:
             self.has_opp = False
-            self.obs, _ , self.done, _ = self.env.reset(np.array([[sx, sy, stheta]]))
+            self.obs, _ = self.env.reset(options={"poses": np.array([[sx, sy, stheta]])})
             self.ego_scan = list(self.obs['scans'][0])
 
         # sim physical step timer
@@ -233,9 +233,9 @@ class GymBridge(Node):
         _, _, rtheta = euler.quat2euler([rqw, rqx, rqy, rqz], axes='sxyz')
         if self.has_opp:
             opp_pose = [self.obs['poses_x'][1], self.obs['poses_y'][1], self.obs['poses_theta'][1]]
-            self.obs, _ , self.done, _ = self.env.reset(np.array([[rx, ry, rtheta], opp_pose]))
+            self.obs, _ = self.env.reset(options={"poses": np.array([[rx, ry, rtheta], opp_pose])})
         else:
-            self.obs, _ , self.done, _ = self.env.reset(np.array([[rx, ry, rtheta]]))
+            self.obs, _ = self.env.reset(options={"poses": np.array([[rx, ry, rtheta]])})
 
     def opp_reset_callback(self, pose_msg):
         if self.has_opp:
@@ -246,7 +246,7 @@ class GymBridge(Node):
             rqz = pose_msg.pose.orientation.z
             rqw = pose_msg.pose.orientation.w
             _, _, rtheta = euler.quat2euler([rqw, rqx, rqy, rqz], axes='sxyz')
-            self.obs, _ , self.done, _ = self.env.reset(np.array([list(self.ego_pose), [rx, ry, rtheta]]))
+            self.obs, _ = self.env.reset(options={"poses": np.array([(self.ego_pose), [rx, ry, rtheta]])})
     def teleop_callback(self, twist_msg):
         if not self.ego_drive_published:
             self.ego_drive_published = True
@@ -278,7 +278,8 @@ class GymBridge(Node):
         scan.angle_max = self.angle_max
         scan.angle_increment = self.angle_inc
         scan.range_min = 0.
-        scan.range_max = 30.
+        # convert each element to float from numpy.float32
+        self.ego_scan = [float(x) for x in self.ego_scan]
         scan.ranges = self.ego_scan
         self.ego_scan_pub.publish(scan)
 
